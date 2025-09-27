b4867b4c10c60dc77f4d3f576f55f3e0efca1554
Daniwiiu <shark10869@sharkcoders.pt>
2025-09-27 10:33:04 +0100

    Add files via upload
    Operações matemáticas

---
+++ Exercício1.py
@@ -0,0 +1,19 @@
+n1 = 3
+n2 = 2
+soma = n1+n2 
+print("A soma dá:",soma)
+
+n3 = 16
+n4 = 7
+subtração = n3-n4
+print("A subtração dá:",subtração)
+
+n5 = 12
+n6 = 4
+divisão = n5/n6
+print("A divisão é:",divisão)
+
+n7 = 22
+n8 = 15
+multiplicação = n7*n8
+print("A multiplicação é:",multiplicação)
